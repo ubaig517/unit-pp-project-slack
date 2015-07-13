48176c29540cb2cca20a8fa773de309826b25699
jgiang <jasminegiang@MacBook-Pro.local>
2015-07-13 11:41:45 -0700

    add tests

--- test/online-calendar-test.js
+++ test/online-calendar-test.js
@@ -1,21 +1,14 @@
 describe('Online-Calendar', function() {
-  var data;
-  var events;
-  var days;
+  var data = [];
+  var events = [];
+  var days = {};
   before(function(done) {
-    $.ajax({
-        method: 'GET',
-        url: 'https://www.googleapis.com/calendar/v3/calendars/0uj8ecocvpma1uqq5gs9lfut0s%40group.calendar.google.com/events?timeMin=2015-06-01T00%3A00%3A00Z&alwaysIncludeEmail=false&showDeleted=false&fields=items&key=AIzaSyAW3CXP4J_8cHT41mi-W_Rt1sRPnHAZS2s'}).then(function(payLoad) {
-      data = payLoad.items;
-      events = data.filter(function(event) {return event.status === "confirmed";});
-      days = {};
-      events.forEach(function(event) {days[event.start.dateTime.slice(0, 10)] = 0});  
-      done();
-    });
-     });
+ doStuff(done, data, events, days);
+  });
+
   it('should have right amount of .event elements', function() {
     expect(document.querySelector(".event")).to.not.equal(null);
-    expect(document.querySelectorAll(".event").length).to.be.greaterThan(events.length);
+    expect(document.querySelectorAll(".event").length).to.be.greaterThan(events.length - 1);
   }); 
   xit('should have only confirmed .event elements', function() {
     expect(document.querySelector(".event")).to.not.equal(null);
@@ -25,6 +18,7 @@ describe('Online-Calendar', function() {
     expect(document.querySelector(".day")).to.not.equal(null);
     expect(document.querySelectorAll(".day").length).to.be.greaterThan(Object.keys(days).length - 1);
   });
+
  });
 mocha.globals();
 mochaPhantomJS.run();
--- test/polyfill.js
+++ test/polyfill.js
@@ -6,3 +6,14 @@ if (window.mochaPhantomJS) {
       };
   };
 }
+
+function doStuff(done, data, events, days) {
+    $.ajax({
+        method: 'GET',
+        url: 'https://www.googleapis.com/calendar/v3/calendars/0uj8ecocvpma1uqq5gs9lfut0s%40group.calendar.google.com/events?timeMin=2015-06-01T00%3A00%3A00Z&alwaysIncludeEmail=false&showDeleted=false&fields=items&key=AIzaSyAW3CXP4J_8cHT41mi-W_Rt1sRPnHAZS2s'}).then(function(payLoad) {
+      data = payLoad.items;
+      events = data.filter(function(event) {return event.status === "confirmed";});
+      events.forEach(function(event) {days[event.start.dateTime.slice(0, 10)] = 0;});  
+      done();
+    });
+}
