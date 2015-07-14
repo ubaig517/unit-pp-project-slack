b51cc02bec8db135d3e1e9c200d03dde2a0afa73
jgiang <jasminegiang@MacBook-Pro.local>
2015-07-13 19:22:17 -0700

    change calendar link

--- test/polyfill.js
+++ test/polyfill.js
@@ -10,7 +10,7 @@ if (window.mochaPhantomJS) {
 function doStuff(done, data, events, days) {
     $.ajax({
         method: 'GET',
-        url: 'https://www.googleapis.com/calendar/v3/calendars/0uj8ecocvpma1uqq5gs9lfut0s%40group.calendar.google.com/events?timeMin=2015-06-01T00%3A00%3A00Z&alwaysIncludeEmail=false&showDeleted=false&fields=items&key=AIzaSyAW3CXP4J_8cHT41mi-W_Rt1sRPnHAZS2s'}).then(function(payLoad) {
+        url: 'https://www.googleapis.com/calendar/v3/calendars/pe13s26rvf4fud47l08ti750i0@group.calendar.google.com/events?key=AIzaSyAyucc-d1nuZQnRsbMeZ1RtP04ZIdKr0qU'}).then(function(payLoad) {
       data = payLoad.items;
       events = data.filter(function(event) {return event.status === "confirmed";});
       events.forEach(function(event) {days[event.start.dateTime.slice(0, 10)] = 0;});  
