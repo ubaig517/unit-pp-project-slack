1e5d96d13cd9ce1e0c6d2a951f6ccc6d18569d93
jgiang <jasminegiang@MacBook-Pro.local>
2015-07-15 16:28:33 -0700

    hardcode numbers in test

--- test/online-calendar-test.js
+++ test/online-calendar-test.js
@@ -8,15 +8,15 @@ describe('Online-Calendar', function() {
 
   it('should have right amount of .event elements', function() {
     expect(document.querySelector(".event")).to.not.equal(null);
-    expect(document.querySelectorAll(".event").length).to.be.greaterThan(events.length - 1);
+    expect(document.querySelectorAll(".event").length).to.be.greaterThan(112);
   }); 
   xit('should have only confirmed .event elements', function() {
     expect(document.querySelector(".event")).to.not.equal(null);
-    expect(document.querySelectorAll(".event").length).to.equal(events.length);
+    expect(document.querySelectorAll(".event").length).to.be.lessThan(123);
   });
   it('should have right amount of .day elements',function(){
     expect(document.querySelector(".day")).to.not.equal(null);
-    expect(document.querySelectorAll(".day").length).to.be.greaterThan(Object.keys(days).length - 1);
+    expect(document.querySelectorAll(".day").length).to.be.greaterThan(16);
   });
 
  });
