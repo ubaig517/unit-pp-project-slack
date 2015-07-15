ae45765879eaf648245db4c3e06d9efeaff6c9ac
jgiang <jasminegiang@MacBook-Pro.local>
2015-07-15 16:42:55 -0700

    add wait time in test

--- test/online-calendar-test.js
+++ test/online-calendar-test.js
@@ -1,13 +1,13 @@
 describe('Online-Calendar', function() {
   before(function(done) {
-    done();
+    setTimeout(function(){done();}, 1000);
   });
 
   it('should have right amount of .event elements', function() {
     expect(document.querySelector(".event")).to.not.equal(null);
     expect(document.querySelectorAll(".event").length).to.be.greaterThan(112);
   }); 
-  xit('should have only confirmed .event elements', function() {
+  it('should have only confirmed .event elements', function() {
     expect(document.querySelector(".event")).to.not.equal(null);
     expect(document.querySelectorAll(".event").length).to.be.lessThan(123);
   });
