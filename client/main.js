0e810338d097505762de91b4a85fd839d56be81a
davidpetri <petridw@gmail.com>
2015-11-12 18:01:52 -0800

    Remove given ajax request.

--- client/main.js
+++ client/main.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-// The following code appends a title to the page
-// document.createElement creates an element that can be altered and then inserted into the DOM
-// document.body.appendChild places a node as a child under the body element
-var title = document.createElement('div');
-title.innerHTML = 'Social Calendar';
-document.body.appendChild(title);
-
-// Your schedule can be accessed through the global object "schedule"
-console.log(schedule);
-=======
 // jquery is available in this file if you bower installed properly
 
 function Calendar(schedule) {
@@ -43,8 +32,8 @@ $(document).on('ready', function() {
   var title = $('<h1>').text('Social Calendar');
   $('body').append(title);
   console.log(title);
-  $.get('http://slack-server.elasticbeanstalk.com/calendar/3').success(function(data) {
-    console.log(data);
-  });
+  
+  // Make an AJAX request to the server, then do something with the result!
+  // ...
+  
 });
->>>>>>> 8745f725863dac8b74c607cdeab638bc9e0176b7
