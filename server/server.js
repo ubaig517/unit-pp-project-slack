e560a706f1a71924788bf5220aed654efd78d2af
Michael Laythe <mrlaythe24@aol.com>
2016-10-04 21:07:27 -0700

    es6ify

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../' ))); //serves the index.html
 app.listen(3000); //listens on port 3000 -> http://localhost:3000/
