f0923b98d82cb30066a3e35d0385c3ee822d0023
Michael Laythe <mrlaythe24@aol.com>
2016-10-04 21:07:42 -0700

    es6ify with classes

--- client/main.js
+++ client/main.js
@@ -1,39 +1,45 @@
-// jquery is available in this file if you bower installed properly
+class Calendar {
+  //do something with the data here
+  constructor(schedule) {
 
-function Calendar(schedule) {
-  // do something with the data here;
-}
-function Event(data) {
-  this.startTime = data.startTime;
-  this.endTime = data.endTime;
-  this.description = data.description;
-}
-function Day(name) {
-  this.name = name;
-  this.events = [];
-  this.addEvent = function(eventData) {
-    this.events.push(new Event(eventData));
-  };
-  this.orderEvents = function() {
-    //go through events and sort them by start and end time
-  };
-}
-function Week(number) {
-  this.number = number;
-  this.days = [];
-  this.addDay = function(name) {
-    var day = new Day(name);
-    this.days.push(day);
-  };
+  }
 }
 
+class Event {
+  constructor(data) {
+    this.startTime = data.startTime;
+    this.endTime = data.endTime;
+    this.description = data.description
+  }
+}
 
-$(document).on('ready', function() {
-  var title = $('<h1>').text('Social Calendar');
+class Day {
+  constructor(name) {
+    this.name = name;
+    this.events = [];
+    this.addEvent = (eventData) => {
+      this.events.push(new Event(eventData));
+    }
+    this.orderEvents = function() {
+      //go through events and sort them by start and end time
+    }
+  }
+}
+
+class Week {
+  constructor(number) {
+    this.days = [];
+    this.addDay = (name) => {
+      const day = new Day(name);
+      this.days.push(day);
+    }
+  }
+}
+
+$(document).on('ready', () => {
+  const title = $('<h1>').text('Social Calendar');
   $('body').append(title);
   console.log(title);
   
   // Make an AJAX request to the server, then do something with the result!
-  // ...
-  
 });
